c85e247ee59b372dcd6978b3ace5daf24e800748
wyattYousey <quiz.nv1@gmail.com>
2025-12-12 16:29:55 -0600

    Created Header Component
    Moved main entry point to project root, restructured components into dedicated folders, and added initial scaffolding for App, Header, and supporting components. Updated index.html and index.css to use new structure and normalize styles. Added configuration files for editor, linting, and formatting. Removed Vite/React starter files and assets.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../Header/Header';
+import './App.css';
+
+function App() {
+
+  return (
+   <div className='app'>
+    <div className="page__content">
+      <Header />
+    </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import avatar from "../../assets/avatar.png";
+import logo from "../../assets/wtwr-logo.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="WTWR Logo" />
+      <p className="header__date-and-location">June 15, New York</p>
+      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <div className="header__user-wrapper">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Avatar Photo" className="header__avatar" />
+      </div>
+    </header>
+  )
+}
+
+export default Header;
